70f5cb8d4548c89d1a1cfa2d67fb425dfaaf2466
dilanpv <dilan.pereava@amigo.edu.co>
2025-09-08 18:11:39 -0500

    Ejercicio de practica

---
+++ Practicas/practicando.py
@@ -0,0 +1,36 @@
+# #Escribe un programa que reciba un número entero y diga si es positivo, negativo o cero.
+# print("Saber si un numero es positivo, negativo o cero:")
+# print("\n")
+
+# opc = 0
+# numero = 0
+# while opc != 4:
+#   print("1. positivo")
+#   print("2. negativo")
+#   print("3. igual acero")
+#   print("4. salir")
+
+#   opc = int(input("Digite la opcion:"))
+#   numero = int(input("Digite el numero:"))
+#   if opc ==1 :
+#     if numero >0:
+#       print("Su numero es positivo")
+#     else:
+#       print("Su numero es negativo")
+  
+#   elif opc == 2:
+#     if numero <0 :
+#       print("Su numero es negativo")
+#     else:
+#       print("Su numero es positivo")
+
+#   elif opc == 3:
+#     if numero == 0 :
+#       print("Su numero es cero")
+#     else:
+#       print("Su numero no es cero 0")
+
+#   elif opc == 4:
+#     print("Salio")
+#   else:
+#     print("Se equivico de opcion crack")
